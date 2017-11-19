2375c80116689c17096c3074af4c4c1e6e31f9a7
Kyle Pastor <kyleanthonypastor@gmail.com>
2017-11-19 11:15:10 -0500

    Updates and basic functionality
    Got the basics working!

--- js/drlTbl.js
+++ js/drlTbl.js
@@ -1,31 +1,50 @@
 (function($) {
 
-    $.fn.drlTbl = function() {
-        // For the table given we need to take the first column as the depth.
-        // Keep this list of depths in the rows:
-        var depth_set = [];
-        $.each(this.find(), function (i, item) {
-          depth_set.push($(item).attr("depth"));
-        });
-        console.log(depth_set);
-        // Now that we have the depth set we can hide anything other than 1
+    $.fn.drlTbl = function(options) {
+      // Define the default options for the plugin:
+      var defaults = {
+        prepend: ">",
+        fontScaling: 2
+      };
+      var options = $.extend(defaults, options);
 
-       this.find("tr[depth][depth!='1']").hide();
+      // First thing to do is to prepend at every depth the prepnd option
+      $.each(this.find("tr[depth]"), function (i, item) {
+        let prepend_str = Array(parseInt($(item).attr("depth"))).join(options.prepend);
+        let cur_size = parseInt($(item).css('font-size'));
+        $(item).css('font-size', (cur_size-options.fontScaling*(parseInt($(item).attr("depth"))-1)).toString()+'px')
+        // For each depth we will prepend the string onto the first td element of the row.
+        $(item).find('td:first').prepend(prepend_str);
+      });
+
+      // Hide all of the level 1 depths
+      this.find("tr[depth][depth!='1']").hide();
 
        // Finally we will bind a click event to show the subrows based on the click
        this.find("tr[depth]").click(function(){
+         let current_depth = parseInt($(this).attr("depth"));
+         // Find out the depth of the next row index
+         let next_row_depth = parseInt($(this).closest('tr').next('tr').attr("depth"));
+         console.log(current_depth);
+         console.log(next_row_depth);
 
-         if ($(this).is(':visible')){
-
-         }else{
-           let current_depth = parseInt($(this).attr("depth"));
-           $.each($(this).nextUntil("tr[depth='"+current_depth+"']"),function(idx,row){
-             if($(row).attr("depth") == current_depth+1){
-               $(row).show();
-             }
-           });
+         // If the next row is not smaller than the current we know we are at a leaf
+         if (current_depth<next_row_depth){// We are able to drill down more now
+           // Check if the next row is already visible.
+           if ($(this).closest('tr').next('tr').is(':visible')){ // Already open so close
+             $.each($(this).nextUntil("tr[depth='"+current_depth+"']"),function(idx,row){
+               if($(row).attr("depth") > current_depth){
+                 $(row).hide();
+               }
+             });
+           }else{ // Not visible so we will open it
+             $.each($(this).nextUntil("tr[depth='"+current_depth+"']"),function(idx,row){
+               if($(row).attr("depth") == current_depth+1){
+                 $(row).show();
+               }
+             });
+           }
          }
-
 
 
        })
@@ -33,9 +52,3 @@
     }
 
 }(jQuery));
-
-
-// $("#tableId > tbody > tr").each(function(){
-//     var name = $(this).find("td:first").text();
-//     console.log("Method name: "+name);
-// });
