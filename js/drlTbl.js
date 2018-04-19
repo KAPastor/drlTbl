820b697f29f826054ae60f8db776e719441a153e
Kyle Pastor <kyleanthonypastor@gmail.com>
2018-04-19 09:02:54 -0400

    a

--- js/drlTbl.js
+++ js/drlTbl.js
@@ -48,16 +48,17 @@
 					});
 				}
 
-				//
-				// // Look for the td drlprepend, drlformat, drlappend stylings
-				// $.each($(item).find("td[drlappend]"), function (j, td_item) {
-				// 	$(td_item).text($(td_item).text() + $(td_item).attr('drlappend'));
-				// });
-				// $.each($(item).find("td[drlprepend]"), function (j, td_item) {
-				// 	$(td_item).text($(td_item).attr('drlprepend') + $(td_item).text());
-				// });
+
+
 			});
 
+			// // Look for the td drlprepend, drlformat, drlappend stylings
+			// $.each($(item).find("td[drlappend]"), function (j, td_item) {
+			// 	$(td_item).text($(td_item).text() + $(td_item).attr('drlappend'));
+			// });
+			// $.each($(item).find("td[drlprepend]"), function (j, td_item) {
+			// 	$(td_item).text($(td_item).attr('drlprepend') + $(td_item).text());
+			// });
 
 		}
 
@@ -67,24 +68,25 @@
 	  this.find("tr[depth][depth!='1']").hide();
 
 	   // Finally we will bind a click event to show the subrows based on the click
-	   this.find("tr[depth]").click(function(){
-		 let current_depth = parseInt($(this).attr("depth"));
+	   this.find("tr[depth] > td:nth-child(1)").click(function(){
+		 let that = $(this).parent('tr');
+		 let current_depth = parseInt(that.attr("depth"));
 		 // Find out the depth of the next row index
-		 let next_row_depth = parseInt($(this).closest('tr').next('tr').attr("depth"));
+		 let next_row_depth = parseInt(that.closest('tr').next('tr').attr("depth"));
 		 console.log(current_depth);
 		 console.log(next_row_depth);
 
 		 // If the next row is not smaller than the current we know we are at a leaf
 		 if (current_depth<next_row_depth){// We are able to drill down more now
 		   // Check if the next row is already visible.
-		   if ($(this).closest('tr').next('tr').is(':visible')){ // Already open so close
-			 $.each($(this).nextUntil("tr[depth='"+current_depth+"']"),function(idx,row){
+		   if (that.closest('tr').next('tr').is(':visible')){ // Already open so close
+			 $.each(that.nextUntil("tr[depth='"+current_depth+"']"),function(idx,row){
 			   if($(row).attr("depth") > current_depth){
 				 $(row).hide();
 			   }
 			 });
 		   }else{ // Not visible so we will open it
-			 $.each($(this).nextUntil("tr[depth='"+current_depth+"']"),function(idx,row){
+			 $.each(that.nextUntil("tr[depth='"+current_depth+"']"),function(idx,row){
 			   if($(row).attr("depth") == current_depth+1){
 				 $(row).show();
 			   }
